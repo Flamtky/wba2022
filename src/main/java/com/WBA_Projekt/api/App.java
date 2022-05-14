3ac74c9d409dc4e6e3fa7ac44bd5219f9f908ffe
Flamtky <n.schumann@rz-online.de>
2022-05-14 19:28:59 +0200

    added startpage html

--- src/main/java/com/WBA_Projekt/api/App.java
+++ src/main/java/com/WBA_Projekt/api/App.java
@@ -2,8 +2,8 @@ package com.WBA_Projekt.api;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
+import java.util.Set;
 
 @ApplicationPath("/api")
 public class App extends Application {
-
 }
